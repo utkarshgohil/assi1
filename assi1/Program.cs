7e55d47e91c462411580debd73bb60bd3a85a0ab
GOHIL Utkarsh <UGL1@dsone.3ds.com>
2022-07-28 15:28:07 +0530

    Add project files.

---
+++ assi1/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Utkarsh");
+
+            //int a = 10;
+            //int b = 20;
+
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            //Console.WriteLine("Value of a & b is " +a +" & "+b);
+
+            //Console.WriteLine("Enter two nos ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Addition is " + (a + b));
+            //Console.WriteLine("Substraction is " + (a - b));
+            //Console.WriteLine("Multiplication is " + (a * b));
+            //Console.WriteLine("Division is " + (a / b));
+
+            //Console.WriteLine("Enter no: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(+a+" x "+i +" = "+(a*i));
+            //}
+
+            //Console.WriteLine("Enter the number");
+            //int no = Convert.ToInt32(Console.ReadLine());
+            //for(int i = 0; i < 4; i++)
+            //{
+            //    Console.Write(+no+" ");
+            //}
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.Write(+no);
+            //}
+
+            //Console.WriteLine("Enter the string");
+            //string str = Console.ReadLine();
+            //int n = str.Length;
+            //char s = str[n-1];
+            //char e = str[0];
+            //string str1=str.Substring(1,n-2);
+            //Console.WriteLine(s+str1+e);
+
+            //Console.WriteLine("Enter two nos");
+            //int m = Convert.ToInt32(Console.ReadLine());
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //if((m>0 && n<0) || (m<0 && n > 0))
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else Console.WriteLine("false");
+
+            //string str = "advadnv aidvadvnao adcnadivnaonnhtf aa cjckm";
+            //int n = 0, max = 0;
+            //string maxw = "", str1 = "";
+            //foreach (char c in str)
+            //{
+            //    if (c == ' ')
+            //    {
+            //        if (n < max)
+            //        {
+            //            n = max;
+            //            max = 0;
+            //            maxw = str1;
+            //            str1 = "";
+            //        }
+            //        else
+            //        {
+            //            max = 0;
+            //            str1 = "";
+            //        }
+            //    }
+            //    else
+            //    {
+            //        str1 = str1 + c.ToString();
+            //        max++;
+            //    }
+            //}
+            //Console.WriteLine("longest word is " + str1);
+
+
+            //Console.WriteLine("Enter a integer no ");
+            //int no = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //while (no != 0)
+            //{
+            //    int rem = no % 10;
+            //    sum += rem;
+            //    no = no / 10;
+            //}
+            //Console.WriteLine(+sum);
+
+            //int[] arr = { 1, 2, 8 };
+            //int temp = arr[0], i;
+            //for (i = 0; i < 2; i++)
+            //{
+            //    arr[i] = arr[i + 1];
+            //}
+
+            //arr[i] = temp;
+
+            //Console.WriteLine("Enter a string ");
+            //string str = Console.ReadLine();
+            //string str1 = str;
+            //str1.Reverse();
+            //if (str1 == str)
+            //{
+            //    Console.WriteLine("Palindrome");
+            //} 
+            //else Console.WriteLine("Not Palindrome");
+
+            //int[] arr1 = { 1, 2, 5 };
+            //int[] arr2 = { 0, 3, 8 };
+            //int[] arr3 = { -1, 0, 2 };
+            //int[] new_array = { arr1[1], arr2[1], arr3[1] };
+            //foreach(int i in new_array)
+            //{
+            //    Console.Write(+i + " ");
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine("Enter the char ");
+            //char c = Convert.ToChar (Console.ReadLine());
+            //Console.WriteLine("ASCII value of char "+(int)c);
+
+            //int j = 100;
+            //string str1 = "abcd";
+            //string jtostr = j.ToString();
+            //int str1toint = Int32.Parse(str1);
+
+            //string str = "abd4538&*";
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    if (str[i] < 'A' || str[i] > 'Z' && str[i] < 'a' || str[i] > 'z')
+            //    {
+            //        str = str.Remove(i, 1);
+            //        i--;
+            //    }
+            //}
+            //Console.WriteLine("string becomes " + str);
+        }
+    }
+}
